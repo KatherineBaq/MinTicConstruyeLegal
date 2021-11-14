37a76bb901592119a425551c35723c6502ab8b1c
Katherine Baquero <kabaquerod@unal.edu.co>
2021-11-14 15:31:10 +0100

    Defined crudAdministrador.js

---
+++ Backend/db/crudAdministrador.js
@@ -0,0 +1,77 @@
+const db = require('./Conection.js');
+
+//Get all Adminsitrators from Administrator collection
+function getAdministradores(callback) {
+    return db.collection('Administrador').get()
+        .then((refDoc) => {
+            var arrayLicencias = [];
+            refDoc.forEach((doc) => {
+                arrayLicencias.push(doc.data());
+            })
+            callback(arrayLicencias);
+        })
+        .catch(err => {
+            callback(`Error to get Admins ${err}`);
+        })
+}
+
+function getAdministrador(aid, callback) {
+    return db.collection('Administrador').doc(aid).get()
+        .then((doc) => {
+            callback(doc.data())
+        })
+        .catch((err) => {
+            callback(`Error to get Admin ${err}`);
+        })
+}
+
+function addAdministrador(administrador, callback) {
+    return db.collection('Administrador').add(administrador)
+        .then(() => {
+            callback("Admin created")
+        })
+        .catch((err) => {
+            callback(`Error to add Admin ${err}`);
+        })
+}
+
+function updateAdminsitradorTotally(aid, administrador, callback) {
+    return db.collection('Administrador').doc(aid).set(administrador)
+        .then(() => {
+            callback("Success");
+        })
+        .catch((err) => {
+            callback(`Error to update Admin ${err}`);
+        })
+}
+
+function updateAdministradorPartial(aid, administrador, callback) {
+    return db.collection('Administrador').doc(aid).update(administrador)
+        .then(() => {
+            callback("Success");
+        })
+        .catch((err) => {
+            callback(`Error to update Admin ${err}`);
+        })
+}
+
+function deleteAdmin(aid, callback) {
+    return db.collection('Administrador').doc(aid).delete()
+        .then(() => {
+            callback("Success");
+        })
+        .catch((err) => {
+            callback(`Error to delete Admin ${err}`);
+        })
+}
+
+//Falta busqueda por lugar
+
+module.exports = {
+    getAdministradores,
+    getAdministrador,
+    addAdministrador,
+    updateAdminsitradorTotally,
+    updateAdministradorPartial,
+    deleteAdmin
+}
